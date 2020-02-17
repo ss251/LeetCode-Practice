27cb2cbdd67395b3ba746cc4d6eab30b92fb0f07
ss251 <ss251@uw.edu>
2020-02-17 02:11:58 -0800

    single folder for all problems

--- Problems/Reverse String.java
+++ Problems/Reverse String.java
@@ -1,6 +1,22 @@
+/*
 344. Reverse String
 
 Write a function that reverses a string. The input string is given as an array of characters char[].
 Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
 You may assume all the characters consist of printable ascii characters.
 https://leetcode.com/problems/reverse-string/
+*/
+
+class Solution {
+  public void helper(char[] s, int left, int right) {
+    if (left >= right) return;
+    char tmp = s[left];
+    s[left++] = s[right];
+    s[right--] = tmp;
+    helper(s, left, right);
+  }
+
+  public void reverseString(char[] s) {
+    helper(s, 0, s.length - 1);
+  }
+}
