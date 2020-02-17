165dfc95eb46a929ea77b4faf09b782eca711920
Kushagra Kumar <kush6357@gmail.com>
2020-02-17 02:49:37 -0800

    104. Maximum Depth of Binary Tree

---
+++ Problems/Maximum Depth of Binary Tree.java
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/
+// 104. Maximum Depth of Binary Tree
+
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return Math.max(left, right) + 1;
+    }
+}
