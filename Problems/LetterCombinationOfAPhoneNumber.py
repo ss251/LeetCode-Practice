ff2c4871f59d22fc2b417dc29675e3a0cc8f107f
aarora0211 <aeora44@uw.edu>
2020-02-19 01:36:14 -0800

    adding to sub_dir

--- Problems/LetterCombinationOfAPhoneNumber.py
+++ Problems/LetterCombinationOfAPhoneNumber.py
@@ -5,16 +5,24 @@ class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         lower_ascii = 94
         comb_dict = dict()
-        comb_list = set()
-        comb_dict[2] = ['a', 'b', 'c']
-        comb_dict[3] = ['d', 'e', 'f']
-        comb_dict[4] = ['g', 'h', 'i']
-        comb_dict[5] = ['j', 'k', 'l']
-        comb_dict[6] = ['m', 'n', 'o']
-        comb_dict[7] = ['p', 'q', 'r', 's']
-        comb_dict[8] = ['t', 'u', 'v']
-        comb_dict[9] = ['w', 'x', 'y', 'z']
-
+        comb_list = []
+        check_rep = set()
+        for s in digits:
+            dig = int(s)
+            if (dig not in check_rep):
+                check_rep.add(dig)
+                start_ascii = lower_ascii + 3 * (dig - 1)
+                for i in range(3):
+                    if dig not in comb_dict:
+                        comb_dict[dig] = [chr(start_ascii + i)]
+                    else:
+                        comb_dict[dig].append(chr(start_ascii + i))
+        if 7 in comb_dict:
+            comb_dict[7].append('s')
+        if 8 in comb_dict:
+            comb_dict[8] = ['t','u','v']
+        if 9 in comb_dict:
+            comb_dict[9] = ['w','x','y','z']
         for i in range(len(digits)):
             comb_list = _helper_add(comb_list, comb_dict[int(digits[i])])
         return comb_list
